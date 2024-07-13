38e7f822eba5830f6350859f7001ea54b63d3042
Kushal Singha <144205096+kushal1004@users.noreply.github.com>
2024-07-13 10:35:37 +0530

    Add files via upload

---
+++ jscript1.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.gift img').addEventListener('click', () => {
+    // Play a birthday song (you can add your own song or use a library like Howler.js)
+    const audio = new Audio('audio/birthday-song.mp3');
+    audio.play();
+     // Animate the balloons 
+    document.querySelectorAll('.balloons img').forEach((img) => {
+        img.style.animationPlayState = 'running';
+    });
+});
